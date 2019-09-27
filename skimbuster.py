caa5d490baa7dff8458f7853fab5ba894e50c625
darumaseye <darumaseye@protonmail.com>
2019-09-27 16:42:30 +0200

    fixed skimbuster.py

--- skimbuster.py
+++ skimbuster.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import sys
+
 import os
-from bluetooth import *
 import bluetooth
 import subprocess
-
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
-import logging
-from waveshare_epd import epd2in13bc
 import time
+import logging
 from PIL import Image,ImageDraw,ImageFont
-import traceback
+
+from lib import epd2in13 as epd_driver
+#from lib import epd2in13 as epd_driver
+
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
 
 logging.basicConfig(level=logging.DEBUG)
 listOfNames = ['HC-05', 'HC-06','DSD TECH HC-05']
@@ -26,31 +22,38 @@ def main():
 	counter = 0
 	try:
 		logging.info("Skimbuster v1.0 Demo")
-		epd = epd2in13bc.EPD()
+		epd = epd_driver.EPD()
 		logging.info("init and Clear")
-		epd.init()
+
+		#epd.init()
+		epd.init(epd.lut_full_update)
+		epd.Clear(0xFF)
+
 		time.sleep(1)
 		font16 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 16)
 		font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-		HBlackimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126
-		HRYimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126  ryimage: red or yellow image
+		HBlackimage = Image.new('1', (epd.EPD_HEIGHT, epd.EPD_WIDTH), 255)  # 298*126
+		#HRYimage = Image.new('1', (epd.EPD_HEIGHT, epd.EPD_WIDTH), 255)  # 298*126  ryimage: red or yellow image
 		drawblack = ImageDraw.Draw(HBlackimage)
-		drawry = ImageDraw.Draw(HRYimage)
-		drawry.text((2, 0), 'Skimbuster', font = font18, fill = 0)
+		#drawry = ImageDraw.Draw(HRYimage)
+		#drawry.text((2, 0), 'Skimbuster', font = font18, fill = 0)
+		drawblack.text((2, 0), 'Skimbuster', font = font18, fill = 0)
 		drawblack.text((175, 1), 'v1.0', font = font18, fill = 0)
 		drawblack.text((35, 85), 'stutm @ SensePost', font = font16, fill = 0)
 		drawblack.line((0, 20, 298, 20), fill = 0)
 		drawblack.line((0, 85, 298, 85), fill = 0)
-		nearby_devices = discover_devices(lookup_names = True, lookup_class = True)
-		for addr, name, cod in nearby_devices:
+		#nearby_devices = bluetooth.discover_devices(lookup_names = True, lookup_class = True)
+		nearby_devices = bluetooth.discover_devices(lookup_names=True)
+		#for addr, name, cod in nearby_devices:
+		for addr, name in nearby_devices:
 			counter = counter + 1
 			if name in listOfNames:
 				deviceName = name
 				counterChecks = counterChecks+1
 
-			if(hex(cod) == '0x1f00'):
-				deviceCod = hex(cod)
-				counterChecks = counterChecks+1
+			#if(hex(cod) == '0x1f00'):
+			#	deviceCod = hex(cod)
+			#	counterChecks = counterChecks+1
 
 			if addr[:8] in listOfPrefix:
 				deviceMac = addr
@@ -65,20 +68,25 @@ def main():
 			drawblack.text((2, 42), 'No skimmer found. :)', font = font18, fill = 0)
 		
 	
-		epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
+		epd.display(epd.getbuffer(HBlackimage))
+		#epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
 		time.sleep(20)
 		logging.info("Clear...")
-		epd.Clear()
+
+		#epd.Clear()
+		epd.init(epd.lut_full_update)
+		epd.Clear(0xFF)
+
 		epd.sleep()
-		command = "/usr/bin/sudo /sbin/shutdown -r now"
-		process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-		output = process.communicate()[0]
+		#command = "/usr/bin/sudo /sbin/shutdown -r now"
+		#process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+		#output = process.communicate()[0]
 	except IOError as e:
 		logging.info(e)
 	    
 	except KeyboardInterrupt:    
 		logging.info("ctrl + c:")
-		epd2in13bc.epdconfig.module_exit()
+		epd_driver.module_exit()
 		exit()
 	
 
